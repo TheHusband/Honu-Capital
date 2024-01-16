1400d88d7f6438eb50abbe98a3d3e10fc76d3906
John Mason <thehusband18z@gmail.com>
2024-01-16 13:11:07 -0800

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,13 +31,13 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
-              width="192"
-              height="192"
-              quality="95"
+              src="/logos.png"
+              alt="logo"
+              width="500"
+              height="500"
+              quality="100"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-250 w-250 rounded-full object-cover"
             />
           </motion.div>
 
@@ -52,7 +52,6 @@ export default function Intro() {
               duration: 0.7,
             }}
           >
-            👋
           </motion.span>
         </div>
       </div>
