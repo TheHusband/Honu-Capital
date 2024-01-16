9b9d5e9b0ec5e61f47c83c4d2a838ccc63d1459d
John Mason <thehusband18z@gmail.com>
2024-01-16 12:45:15 -0800

    changes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
-};
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
